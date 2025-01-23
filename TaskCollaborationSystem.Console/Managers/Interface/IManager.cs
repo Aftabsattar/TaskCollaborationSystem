257d418c62137bd2d4203898ff12cdc962d2cb68
Zubair Jamil <xubairjamil@gmail.com>
2025-01-23 21:20:16 +0500

    Refactor to use generic manager and add new project
    Updated Program.cs to use a generic IManager interface, allowing management of both Member and MemberTask entities. Added ChooseManager and ChooseEntity methods for user selection. Added new TaskCollaborationSystem.ConsoleApp project with necessary configurations. Introduced Member and MemberTask classes implementing IEntity interface. Added IEntity and IManager interfaces, and a generic Manager class for entity management.

---
+++ TaskCollaborationSystem.Console/Managers/Interface/IManager.cs
@@ -0,0 +1,10 @@
+﻿namespace TaskCollaborationSystem.ConsoleApp.Managers.Interface
+{
+    public interface IManager
+    {
+        void Add();
+        void Remove();
+        void Update();
+        void View();
+    }
+}
---
+++ TaskCollaborationSystem.Console/Managers/Manager.cs
@@ -0,0 +1,76 @@
+﻿using TaskCollaborationSystem.ConsoleApp.Entities.Interface;
+using TaskCollaborationSystem.ConsoleApp.Managers.Interface;
+
+namespace TaskCollaborationSystem.ConsoleApp.Managers
+{
+
+    class Manager<EntityClass> : IManager
+        where EntityClass : IEntity, new()
+    {
+        private List<EntityClass> list = new();
+
+        public void Add()
+        {
+            var obj = new EntityClass();
+
+            obj.InputAndPopulate();
+            list.Add(obj);
+
+            Console.WriteLine($"{obj.Name} added successfully!");
+            PressToContinue();
+        }
+
+        public void Remove()
+        {
+            Console.Write("Enter Name: ");
+            var objName = Console.ReadLine();
+            var obj = list.Find(obj => obj.Name == objName);
+            if (obj == null)
+            {
+                Console.WriteLine("object not found!");
+                PressToContinue();
+                return;
+            }
+            list.Remove(obj);
+            Console.WriteLine($"{objName} Removed Successfully!");
+            PressToContinue();
+        }
+
+        public void Update()
+        {
+            Console.Write("Enter Member Name: ");
+            var objName = Console.ReadLine();
+            var obj = list.Find(obj => obj.Name == objName);
+            if (obj == null)
+            {
+                Console.WriteLine("Member not found!");
+                PressToContinue();
+                return;
+            }
+            obj.InputAndPopulate();
+            Console.WriteLine($"{objName} Updated Successfully!");
+            PressToContinue();
+        }
+
+        public void View()
+        {
+            if (list.Count == 0)
+            {
+                Console.WriteLine("No list found!");
+                PressToContinue();
+                return;
+            }
+            foreach (var obj in list)
+                obj.Show();
+            PressToContinue();
+
+        }
+
+        void PressToContinue()
+        {
+            Console.WriteLine("\nPress any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
+
